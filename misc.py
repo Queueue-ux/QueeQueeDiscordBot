4375e0836a84e6bea605728c7687419ccda5090b
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-11-24 13:57:55 -0800

    Add files via upload
    all random commands with no real category are in the misc class

---
+++ misc.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+import random
+
+class Miscellaneous(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    
+    @commands.command(name='flip',help='flips a coin, either heads or tails')
+    async def flip_coin(self, ctx, flips:int = 1):
+        choices = ['Heads','Tails'] # space for printing purposes
+        result_print = ""
+        if flips > 500000:
+            await ctx.send('Error: Max flips is 500000')
+            return
+
+        results = [random.choice(choices) for i in range(flips)]
+
+        if flips > 1:
+            heads = results.count('Heads')
+            tails = results.count('Tails')
+            result_print += f"\ntotal heads = {heads} ({heads/len(results)})\ntotal tails = {tails} ({tails / len(results)})"
+        else:
+            result_print += results[0]
+
+        await ctx.send(result_print)
