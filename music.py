8963150b2be66e7d9adb4e780c8d54c5a76d7d56
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-11-24 13:58:37 -0800

    Update music.py
    fully updated with cogs

--- music.py
+++ music.py
@@ -6,62 +6,22 @@
 # add ffmpeg install to path if on windows
 
 import os
-import random
-
-import time
 
 from discord.ext import commands
-from discord import utils
-
 from dotenv import load_dotenv
 from FunnyCounter import FunnyCounter
 from music_player import music_player
+from misc import Miscellaneous
+from listeners import listeners
 
 load_dotenv()
 
-
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+
 bot = commands.Bot(command_prefix='!')
-
-
-
-@bot.event
-async def on_ready():
-    guild = utils.get(bot.guilds,name=GUILD)
-
-    print(
-        f'{bot.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-@bot.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
-
-@bot.command(name='flip',help='flips a coin, either heads or tails')
-async def flip_coin(ctx, flips:int = 1):
-    choices = ['Heads','Tails'] # space for printing purposes
-    result_print = ""
-    if flips > 500000:
-        await ctx.send('Error: Max flips is 500000')
-        return
-
-    results = [random.choice(choices) for i in range(flips)]
-
-    if flips > 1:
-        heads = results.count('Heads')
-        tails = results.count('Tails')
-        result_print += f"\ntotal heads = {heads} ({heads/len(results)})\ntotal tails = {tails} ({tails / len(results)})"
-    else:
-        result_print += results[0]
-
-    await ctx.send(result_print)
-
 bot.add_cog(FunnyCounter(bot))
 bot.add_cog(music_player(bot))
+bot.add_cog(Miscellaneous(bot))
+bot.add_cog(listeners(bot,GUILD))
 bot.run(TOKEN)
