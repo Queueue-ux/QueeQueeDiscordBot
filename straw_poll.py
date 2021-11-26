f000d2663f2e1c43aa3856d66da2bef8b44741b9
Quinton <q@gmail.com>
2021-11-25 20:40:11 -0700

    poll edits when someone makes a vote, votes are done by dm to the bot, ending the poll edits the message and determines the winner

--- straw_poll.py
+++ straw_poll.py
@@ -8,7 +8,9 @@ class straw_poll(commands.Cog):
         self.author = None
         self.already_voted = [] # list of everyone who has voted
         self.poll_name = None
+        self.edit_message = None
         self.options = []
+        self.auto_end = False
     
     @commands.Cog.listener()
     async def on_message(self,message):
@@ -25,14 +27,11 @@ class straw_poll(commands.Cog):
                             self.already_voted.append(message.author)
 
                             # next update the poll to show the new vote
-
-                            #first find message in the cache
-                            messages = self.bot.cached_messages._SequenceProxy__proxied
-                            for i in range(len(messages) - 1, -1, -1):
-                                message_content = messages[i].content
-                                strip_message = message_content.split('\n')
-                                if strip_message[0] == "NEW POLL":#found the message we want to edit!
-                                    print(strip_message)
+                            change_index = self.edit_message.content.index(user_vote) + len(user_vote) + 1
+                            number = str(int(self.edit_message.content[change_index]) + 1)
+                            new_message = self.edit_message.content[:change_index] + number + self.edit_message.content[change_index + 1:]
+                            await self.edit_message.edit(content = new_message)
+                            return
                         else:
                             await message.channel.send("invalid option")
 
@@ -47,15 +46,32 @@ class straw_poll(commands.Cog):
             return
         self.is_poll_active = True
         self.poll_name = pollName
+        self.author = ctx.author
         self.options = " ".join(args).strip().split(',')
         self.options = [i.strip() for i in self.options]
         self.options = {i:0 for i in self.options}
         
-        await ctx.send(f"NEW POLL\n{self.poll_name}\n\ncast your vote by DMing QueeQUeeJr:\n !vote " "{option you want}" + "\n\nOPTIONS\n" + "".join([i + " 0\n" for i in self.options]))
-
-
-
-        
+        self.edit_message = await ctx.send(f"NEW POLL\n{self.poll_name}\n\ncast your vote by DMing QueeQUeeJr:\n !vote " "{option you want}" + "\n\nOPTIONS\n" + "".join([i + " 0\n" for i in self.options]))
+    
     @commands.command(name='endPoll',help='create straw poll')
     async def end_poll(self, ctx, *args):
-        pass
+        #make sure the person ending the poll is the one that started it
+        if ctx.author == self.author or self.auto_end:
+            split_message = self.edit_message.content.split()
+            highest = ['option',-100000]
+            for k,v in self.options.items():
+                option_index = split_message.index(k) + 1
+                if int(split_message[option_index]) > highest[1]:
+                    highest[0] = k
+                    highest[1] = int(split_message[option_index])
+            print("hello")
+            final_message = self.edit_message.content[:] + f'\n```JSON\n\n"{highest[0]} WON WITH {highest[1]} VOTES"```'
+            await self.edit_message.edit(content = final_message)
+            self.is_poll_active = False
+            self.author = None
+            self.already_voted = [] # list of everyone who has voted
+            self.poll_name = None
+            self.edit_message = None
+            self.options = []
+        else:
+            await ctx.send("only creater of poll can end it")
