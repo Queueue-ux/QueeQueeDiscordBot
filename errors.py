92b9d734f5fa1928aab95fe04884f6cb0a1e3828
Quinton <q@gmail.com>
2021-11-25 20:40:54 -0700

    error message prints to err.log file

--- errors.py
+++ errors.py
@@ -20,7 +20,7 @@ class ErrorHandling(commands.Cog):
         if isinstance(error, ignored):
             return
         with open('err.log', 'a') as f:
-            traceback.print_exception(type(error),error,error.__traceback__,file=f)
+            traceback.print_exception(type(error),error,error.__traceback__, file=f)
             print(f"\n\ncontext:\n{self._context_reader(ctx)}\n\n\n===========================================\n===========================================\n", file=f)
 
     @commands.command(name='ReportError',help='allows user to report error, describe error the best you can')
