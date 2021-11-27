8def811d61a1c0ec8a224e2fd37f68f9f5a0be9c
Quinton <q@gmail.com>
2021-11-26 18:48:51 -0700

    now prints to console and file one error

--- errors.py
+++ errors.py
@@ -21,7 +21,9 @@ class ErrorHandling(commands.Cog):
             return
         with open('err.log', 'a') as f:
             traceback.print_exception(type(error),error,error.__traceback__, file=f)
+            traceback.print_exception(type(error),error,error.__traceback__)
             print(f"\n\ncontext:\n{self._context_reader(ctx)}\n\n\n===========================================\n===========================================\n", file=f)
+            print(f"\n\ncontext:\n{self._context_reader(ctx)}\n\n\n===========================================\n===========================================\n")
 
     @commands.command(name='ReportError',help='allows user to report error, describe error the best you can')
     async def flip_coin(self, ctx, *args):
